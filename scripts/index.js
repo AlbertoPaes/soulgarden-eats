361aada5fa986ef9ce419f370233c6b26e7346e2
Alberto <tejotaesi@gmail.com>
2023-10-12 22:52:02 -0300

    feat: When finalizing the order, the user confirms the details and is directed to WhatsApp Web, in a conversation with the restaurant's contact, already with a pre-filled standard message

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,16 @@
 let counter = 0;
+let dish;
+let drink;
+let dessert;
 
 
-function selectOrderOption (orderOption,element) {
+function selectOrderOption (orderOption,element,name,price) {
   const orderOptionSelected = document.querySelector(`.${orderOption} .combo-option-selected`);
 
   //This conditional is used to deselect an option from the combo that was already selected.
   if(element.classList.contains('combo-option-selected')){
     element.classList.remove("combo-option-selected");
     counter--;
-    console.log(counter);
     checkOrder();
     return;
   }
@@ -16,17 +18,34 @@ function selectOrderOption (orderOption,element) {
   if(orderOptionSelected !== null) {
     orderOptionSelected.classList.remove("combo-option-selected");
     counter--;
-    console.log(counter);
     checkOrder();
+  }
+
+  if(orderOption === 'dish') {
+    dish = {
+      name,
+      price: price.toFixed(2)
+    };
+  }
+  if(orderOption === 'drink') {
+    drink = {
+      name,
+      price: price.toFixed(2)
+    };
+  }
+  if(orderOption === 'dessert') {
+    dessert = {
+      name,
+      price: price.toFixed(2)
+    };
   }
 
   element.classList.add("combo-option-selected"); 
   counter++;
-  console.log(counter);
   checkOrder();
 }
 
-function checkOrder() {
+function checkOrder () {
   const buttonOrder = document.querySelector("footer button");
 
   if(counter === 3){
@@ -38,4 +57,45 @@ function checkOrder() {
     buttonOrder.style.cssText = "background-color: #CBCBCB;; font-weight: 400; cursor: not-allowed";
     buttonOrder.innerHTML = "Choose the 3 items<br>to finalize the order";
   }
+}
+
+function openModal () {
+  const overlay = document.querySelector(".overlay");
+  overlay.classList.remove("hidden");
+
+  const totalOrder = Number(dish.price) + Number(drink.price) + Number(dessert.price);
+
+  document.querySelector(".order-dish-name").innerHTML = dish.name;
+  document.querySelector(".order-drink-name").innerHTML = drink.name;
+  document.querySelector(".order-dessert-name").innerHTML = dessert.name;
+
+  document.querySelector(".order-dish-price").innerHTML = dish.price;
+  document.querySelector(".order-drink-price").innerHTML = drink.price;
+  document.querySelector(".order-dessert-price").innerHTML = dessert.price;
+
+  document.querySelector(".order-price-total").innerHTML = `$ ${totalOrder.toFixed(2)}`;
+}
+
+function closeModal() {
+  const overlay = document.querySelector(".overlay");
+  overlay.classList.add("hidden");
+}
+
+function sendOrder () {
+  const clientName = prompt("Please enter your name!");
+  const adress = prompt("Please enter delivery location!");
+  const phoneNumber = 5571999999999;
+  const totalPrice = Number(dish.price) + Number(drink.price) + Number(dessert.price);
+  const encodedText = `
+  Hello, I would like to place an order:\n
+  - Dish: ${dish.name}\n
+  - Drink: ${drink.name}\n
+  - Dessert: ${dessert.name}\n
+  Total: R$ ${totalPrice.toFixed(2)}
+
+  Nome: ${clientName}
+  Endereço: ${adress}
+  `;
+  const urlWhatsapp = `https://wa.me/${phoneNumber}?text=${encodedText}`;
+  window.open(urlWhatsapp);
 }
